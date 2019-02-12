2b9171179080fa5f93172532d6284e8c56ad9651
cunjunwang <Pass234>
2019-02-12 16:13:00 +0800

    【001】Finish;

---
+++ src/main/java/com/cunjunwang/leetcodejava/solutions/solution001_TwoSum/TwoSum.java
@@ -0,0 +1,55 @@
+package com.cunjunwang.leetcodejava.solutions.solution001_TwoSum;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Create by cunjunwang on 2019/2/12
+ */
+@Component
+public class TwoSum {
+
+    /**
+     * Given an array of integers, return indices of the two numbers such that they add up to a specific target.
+     * <p>
+     * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+     * <p>
+     * Example:
+     * <p>
+     * Given nums = [2, 7, 11, 15], target = 9,
+     * <p>
+     * Because nums[0] + nums[1] = 2 + 7 = 9,
+     * return [0, 1].
+     *
+     * @param nums
+     * @param target
+     * @return
+     */
+    public int[] twoSumBF(int[] nums, int target) {
+        int[] result = new int[2];
+        for (int i = 0; i < nums.length; i++) {
+            for (int j = i + 1; j < nums.length; j++) {
+                if (nums[i] + nums[j] == target) {
+                    result[0] = i;
+                    result[1] = j;
+                    return result;
+                }
+            }
+        }
+        return result;
+    }
+
+    public int[] twoSumOn(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.get(target - nums[i]) != null) {
+                return new int[]{map.get(target - nums[i]), i};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[]{0, 0};
+    }
+
+}
---
+++ src/test/java/com/cunjunwang/leetcodejava/solutions/solution001_TwoSum/TwoSumTest.java
@@ -0,0 +1,29 @@
+package com.cunjunwang.leetcodejava.solutions.solution001_TwoSum;
+
+import com.cunjunwang.leetcodejava.LeetCodeJavaApplicationTests;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import static org.junit.Assert.*;
+
+/**
+ * Create by cunjunwang on 2019/2/12
+ */
+public class TwoSumTest extends LeetCodeJavaApplicationTests {
+
+    private static final Logger logger = LoggerFactory.getLogger("Logger");
+
+    @Autowired
+    private TwoSum twoSum;
+
+    @Test
+    public void twoSumBF() {
+        logger.info("测试开始");
+        int[] input = {2, 7, 11, 15};
+        int target = 9;
+        int[] result = twoSum.twoSumOn(input, target);
+        logger.info("测试完成, 结果: {}", result);
+    }
+}
