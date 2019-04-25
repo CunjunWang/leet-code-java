8ab84347645352eae16fab8bc4b50c7fab6d1f7f
Cunjun Wang <13621691063@163.com>
2019-04-25 14:15:36 +0800

    【070】Finished

---
+++ src/main/java/com/cunjunwang/leetcodejava/solutions/solution70_ClimbingStairs/ClimbingStairs.java
@@ -0,0 +1,78 @@
+package com.cunjunwang.leetcodejava.solutions.solution70_ClimbingStairs;
+
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by CunjunWang on 2019-04-25.
+ */
+@Component
+public class ClimbingStairs {
+
+    /**
+     * You are climbing a stair case.
+     * It takes n steps to reach to the top.
+     * Each time you can either climb 1 or 2 steps.
+     * In how many distinct ways can you climb to the top?
+     * <p>
+     * Note: Given n will be a positive integer.
+     * <p>
+     * Example 1:
+     * Input: 2
+     * Output: 2
+     * Explanation: There are two ways to climb to the top.
+     * 1. 1 step + 1 step
+     * 2. 2 steps
+     * <p>
+     * Example 2:
+     * Input: 3
+     * Output: 3
+     * Explanation: There are three ways to climb to the top.
+     * 1. 1 step + 1 step + 1 step
+     * 2. 1 step + 2 steps
+     * 3. 2 steps + 1 step
+     *
+     * @param n
+     * @return
+     */
+//    public int climbStairs(int n) {
+//        int result = 0;
+//        int totalNumberOfTwo = Math.floorDiv(n, 2);
+//        for (int numberOfTwo = 0; numberOfTwo <= totalNumberOfTwo; numberOfTwo++) {
+//            int numberOfOne = n - numberOfTwo * 2;
+//            result = result + this.combination(numberOfOne, numberOfTwo);
+//        }
+//        return result;
+//    }
+//
+//    private int combination(int numberOfOne, int numberOfTwo) {
+//        int total = numberOfOne + numberOfTwo;
+//        return this.factorial(total, 1)/(this.factorial(numberOfOne, 1)*this.factorial(numberOfTwo, 1));
+//    }
+//
+//    private int factorial(int n, int result) {
+//
+//        if (n == 0) return result;
+//        return factorial(n - 1, n * result);
+//    }
+
+    private Map<Integer, Integer> dp = new HashMap();
+
+    public int climbStairs(int n) {
+        if (n == 0) {
+            return 1;
+        }
+        if (n < 0) {
+            return 0;
+        }
+        if (dp.containsKey(n)) {
+            return dp.get(n);
+        }
+        int count = climbStairs(n - 1) + climbStairs(n - 2);
+        dp.put(n, count);
+        return count;
+    }
+
+}
---
+++ src/test/java/com/cunjunwang/leetcodejava/solutions/solution70_ClimbingStairs/ClimbingStairsTest.java
@@ -0,0 +1,62 @@
+package com.cunjunwang.leetcodejava.solutions.solution70_ClimbingStairs;
+
+import com.cunjunwang.leetcodejava.LeetCodeJavaApplicationTests;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by CunjunWang on 2019-04-25.
+ */
+public class ClimbingStairsTest extends LeetCodeJavaApplicationTests {
+
+    private static final Logger logger = LoggerFactory.getLogger(ClimbingStairsTest.class);
+
+    @Autowired
+    private ClimbingStairs climbingStairs;
+
+    @Test
+    public void climbStairs() {
+//        int n1 = 0;
+//        int result1 = climbingStairs.climbStairs(n1);
+//        logger.info("[{}] result is [{}]", n1, result1);
+//
+//        int n2 = 1;
+//        int result2 = climbingStairs.climbStairs(n2);
+//        logger.info("[{}] result is [{}]", n2, result2);
+//
+//        int n3 = 2;
+//        int result3 = climbingStairs.climbStairs(n3);
+//        logger.info("[{}] result is [{}]", n3, result3);
+//
+//        int n4 = 3;
+//        int result4 = climbingStairs.climbStairs(n4);
+//        logger.info("[{}] result is [{}]", n4, result4);
+//
+//        int n5 = 4;
+//        int result5 = climbingStairs.climbStairs(n5);
+//        logger.info("[{}] result is [{}]", n5, result5);
+//
+//        int n6 = 5;
+//        int result6 = climbingStairs.climbStairs(n6);
+//        logger.info("[{}] result is [{}]", n6, result6);
+//
+//        int n7 = 6;
+//        int result7 = climbingStairs.climbStairs(n7);
+//        logger.info("[{}] result is [{}]", n7, result7);
+//
+//        int n8 = 7;
+//        int result8 = climbingStairs.climbStairs(n8);
+//        logger.info("[{}] result is [{}]", n8, result8);
+//
+//        int n9 = 8;
+//        int result9 = climbingStairs.climbStairs(n9);
+//        logger.info("[{}] result is [{}]", n9, result9);
+//
+        int n10 = 35;
+        int result10 = climbingStairs.climbStairs(n10);
+        logger.info("[{}] result is [{}]", n10, result10);
+
+    }
+}
