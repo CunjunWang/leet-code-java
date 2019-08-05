681a987a34df6f0c3f04d969838cdeedeb777ef6
Cunjun Wang <13621691063@163.com>
2019-08-05 17:36:39 +0800

    011 finishes

---
+++ src/main/java/com/cunjunwang/leetcodejava/solutions/solution011_ContainerWithMostWater/ContainerWithMostWater.java
@@ -0,0 +1,43 @@
+package com.cunjunwang.leetcodejava.solutions.solution011_ContainerWithMostWater;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Created by CunjunWang on 2019-08-05.
+ */
+@Component
+public class ContainerWithMostWater {
+
+    /**
+     * Given n non-negative integers a1, a2, ..., an , where each
+     * represents a point at coordinate (i, ai). n vertical lines
+     * are drawn such that the two endpoints of line i is at (i, ai)
+     * and (i, 0). Find two lines, which together with x-axis forms
+     * a container, such that the container contains the most water.
+     * <p>
+     * Note: You may not slant the container and n is at least 2.
+     * <p>
+     * Example:
+     * Input: [1,8,6,2,5,4,8,3,7]
+     * Output: 49
+     *
+     * @param height
+     * @return
+     */
+    public int maxArea(int[] height) {
+        int left = 0;
+        int right = height.length - 1;
+        int max = 0;
+        while (left < right) {
+            int area = (right - left) * Math.min(height[left], height[right]);
+            max = Math.max(area, max);
+            if (height[left] <= height[right]) {
+                left++;
+            } else {
+                right--;
+            }
+        }
+        return max;
+    }
+
+}
