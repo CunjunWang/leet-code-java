217c3eb404c53b6702493cbebb300f97c3dc35fe
Cunjun Wang <duckwcj@gmail.com>
2019-10-30 12:26:35 -0400

    198

---
+++ src/main/java/com/cunjunwang/leetcodejava/solutions/solution0198_HouseRobber/HouseRobber.java
@@ -0,0 +1,26 @@
+package com.cunjunwang.leetcodejava.solutions.solution0198_HouseRobber;
+
+/**
+ * Created by CunjunWang on 2019-10-30.
+ */
+public class HouseRobber {
+
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return 0;
+        if (nums.length == 1)
+            return nums[0];
+        if (nums.length == 2)
+            return Math.max(nums[0], nums[1]);
+
+        // dp[i] means the largest robbed value till ith house
+        int[] dp = new int[nums.length];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
+        for (int i = 2; i < dp.length; i++) {
+            dp[i] = Math.max(nums[i] + dp[i - 2], dp[i - 1]);
+        }
+
+        return dp[nums.length - 1]
+    }
+}
